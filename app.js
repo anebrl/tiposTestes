27f4c78ca960e2f89e19b29515b8a63329d8ec34
Rosiane <rosi.brito.lima@gmail.com>
2024-09-07 23:14:43 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function pesquisar(){
+
+let section = document.getElementById("resultados-pesquisa");
+
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+if (!campoPesquisa){
+    section.innerHTML = "<p>Busca vazia</p>"
+    return 
+}
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+//console.log(campoPesquisa);
+
+//console.log(section);
+
+let resultados = "";
+let titulo = "";
+let descricao = "";
+let ferramentas = "";
+let tags = "";
+
+    for (let dado of dados){
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        ferramentas = dado.ferramentas.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || ferramentas.includes(campoPesquisa) || tags.includes(campoPesquisa))
+            {
+            resultados += `
+
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}
+                    </a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}
+                </p>
+            </div>
+        `;
+        }
+    
+    //    console.log(dado.titulo.includes(campoPesquisa))
+    }
+    if (!resultados){
+        resultados = "<p>Nada foi encontrado!</p>"
+
+    }
+
+    section.innerHTML = resultados;
+
+}
